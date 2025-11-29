b35ac5f99a17f01d134919c00cec75ea668399c2
big2undey-pixel <big2undey@gmail.com>
2025-11-29 03:13:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 from urllib.parse import urlparse
 import re
 import math
-from collections import CounterUpload Passport Photo
+from collections import Counter
 from datetime import date
 
 # -----------------------------
